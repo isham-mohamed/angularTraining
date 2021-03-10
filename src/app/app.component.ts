ea9247eefab45db522505a9a6964ddbadb411927
isham mohamed <mail.to.isham@gmail.com>
2021-03-10 11:14:21 +0530

    binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,39 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   styleUrls: ['./app.component.scss'],
   template: `
-    <button [id]="id" class="button" [style.backgroundColor]="'red'">Button</button>
-    <input >
+    <input [value]="title1">
+    {{title1}} 
+    <button (click)="changeText()">Change Text</button> -->
+    <table>
+      <tr>
+        <td [attr.colSpan]="colSpanNo">table</td>
+      </tr>
+    </table> -->
+    <button class="button" [class.active]="isActive">Button</button>
+    <button class="button" [style.background-color]="'blue'">Button</button> 
+    <input (keyup.enter)="onKeyUp($event)">
+    <div (click)="onDivClick()">
+      <button (click)="buttonClick($event)">Button</button>
+    </div>
+    <input #inputTag (keyup.enter)="onKeyUp(inputTag.value)">
   `
 })
 export class AppComponent {
-  isActive=false
-  id="btn"
-  imgSrc="https://images.freeimages.com/images/large-previews/867/volcanic-mt-ngauruhoe-1378772.jpg"
-  
+  isActive=true
+  title1="Binding"
+  colSpanNo=2
+  onDivClick(){
+    console.log("div click");
+  }
+  buttonClick($event){
+    $event.stopPropagation();
+    console.log("button click");
+  }
+  changeText(){
+    this.title1="changed Message"
+    console.log(this.title1);
+  }
+  onKeyUp(inputText){
+    console.log(inputText);
+  }
 }
