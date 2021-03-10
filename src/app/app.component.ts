059db81a88fc66254704f3c19b6ad7322e000b99
isham mohamed <mail.to.isham@gmail.com>
2021-03-10 09:03:22 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template:`
+    <h2>{{deviceInfo.deviceName}}</h2>
+    <h2>{{deviceInfo.noOfModels}}</h2>
+    <h2>{{deviceInfo.price}}</h2>
+    <h2>{{deviceInfo.profit}}</h2>
+    <h2>{{deviceInfo.latestReleaseDate}}</h2>
+  `
 })
 export class AppComponent {
-  title = 'angularTraining';
+  deviceInfo={
+    deviceName:"=One Plus",
+    price:1234.5678,
+    profit:120.5678,
+    noOfModels:6524,
+    latestReleaseDate: new Date(2020,3,10)
+  
+  }
 }
