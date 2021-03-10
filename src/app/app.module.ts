a6bad654a779aa369afc131b61fd9285419415e9
isham mohamed <mail.to.isham@gmail.com>
2021-03-10 06:49:54 +0530

    cleaned

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { Home2Component } from './home2/home2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    Home2Component,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
   ],
   providers: [Device],
   bootstrap: [AppComponent]
