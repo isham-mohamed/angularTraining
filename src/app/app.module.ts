7574716493cef0197a53828f166a4e814f45e6a0
isham mohamed <mail.to.isham@gmail.com>
2021-03-16 07:28:52 +0530

    bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { Device } from './device.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
