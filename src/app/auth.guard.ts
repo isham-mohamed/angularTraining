584eb9a82f5a7223fc459619680b37606f12264c
isham mohamed <mail.to.isham@gmail.com>
2021-03-17 08:59:41 +0530

    set

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -16,7 +16,7 @@ export class AuthGuard implements CanActivate {
     if(this.authService.isLoggedIn()){
       return true
     }
-    this.router.navigate([''])
+    this.router.navigate(['login'])
     return false
   }
   
